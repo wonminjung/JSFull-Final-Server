d0a0e120809e0284a24751c71feabf2517bb79d5
maruanna <maruanna1994@gmail.com>
2024-06-17 16:46:53 +0900

    userId import 추가

--- models/bookingSchema.js
+++ models/bookingSchema.js
@@ -1,15 +1,44 @@
 import { Schema, model } from "mongoose";
 import { getCurrentTime } from "../utils/utils";
+import { ObjectId } from "mongodb";
 
 const bookingSchema = new Schema({
-    checkInDate : {type: Date, required: true },
-    checkOutDate : {type: Date, required: true  },
-    guests:{type: Number,required: true },
-    infants:{type: Number },
-    userId : { type: ObjectId ,ref: "User", Unique: true, required:true},
-    roomId : { type: ObjectId, ref: "Room",Unique: true, required:true },
-    createdAt : { type : String, default : getCurrentTime },
-    updatedAt : { type : String, default : getCurrentTime},
+    guests: {
+        type: Number,
+        required: true
+    },
+    infants: {
+        type: Number
+    },
+    userId : { 
+        type: ObjectId,
+         ref: "User", 
+         Unique: true, 
+         required:true
+    },
+    roomId : { 
+        type: ObjectId, 
+        ref: "Room",
+        Unique: true, 
+        required:true 
+    },
+    createdAt : { 
+        type : String, 
+        default : getCurrentTime 
+    },
+    updatedAt : { 
+        type : String, 
+        default : getCurrentTime
+    },
+
+    checkInDate : {
+        type: Date, 
+        required: true 
+    },
+    checkOutDate : {
+        type: Date, 
+        required: true  
+    },
 });
 
 export default model("Booking", bookingSchema, "booking");
--- models/commentSchema.js
+++ models/commentSchema.js
@@ -3,13 +3,35 @@ import { getCurrentTime } from "../utils/utils";
 import { ObjectId } from "mongodb";
 
 const commentSchema = new Schema({
-    userId : {type: String, required : true},
-    roomId : {type: Number, required : true},
-    body : {type: String, required : true},
-    star : {type: Number, required : true, default: 0},
-    grade : {type: ObjectId, required : true},
-    createAt: {type: String, default : getCurrentTime},
-    updateAt: {type: String, default : getCurrentTime},
+    userId : {
+        type: String, 
+        required : true
+    },
+    roomId : {
+        type: Number, 
+        required : true
+    },
+    body : {
+        type: String, 
+        required : true
+    },
+    star : {
+        type: Number, 
+        required : true, 
+        default: 0
+    },
+    grade : {
+        type: ObjectId, 
+        required : true
+    },
+    createAt: {
+        type: String, 
+        default : getCurrentTime
+    },
+    updateAt: {
+        type: String, 
+        default : getCurrentTime
+    },
 });
 
 export default model("Comment", commentSchema, "comment");
