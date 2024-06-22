b0aae0b41aa57babb8970b449a7741e0e81b078c
“wmj” <tmanfektjt25@gmail.com>
2024-06-23 00:57:24 +0900

    app.js 에 cors 추가

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import connect from "./connect/connect.js";
 import rootRouter from "./routes/rootRouter.js";
-
+import cors from 'cors';
 
 // 몽고DB
 connect();
 
 const app = express();
 const port = 8000;
+
+app.use(cors());
 
 app.use("/", rootRouter);
 
