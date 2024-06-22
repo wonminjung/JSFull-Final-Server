4f63b41eeabbd3c91fc69183847cb46c1a80b09a
maruanna <maruanna1994@gmail.com>
2024-06-23 00:17:15 +0900

    booking데이터 완료

--- createData/bookingData/createBooking.js
+++ createData/bookingData/createBooking.js
@@ -18,16 +18,16 @@ const user9 = await User.findOne({userId : "LeeWM"});
 const user10 = await User.findOne({userId : "KwonHW"}); 
 
 // room
-const room1 = await Room.findOne({_id : "6671baaa295d664b248c586a"}); //위드독 독채 풀빌라 -6
-const room2 = await Room.findOne({_id : "6672f5a8424b440bf6b300fd"}); //강화도 럭셔리 대형 풀빌라-16
-const room3 = await Room.findOne({_id : "6672f5a8424b440bf6b30116"}); //다온빌(스파/개별풀)-10
-const room4 = await Room.findOne({_id : "6672f5a8424b440bf6b3016b"}); //바닷가 바로앞 오션뷰스파 객실, b200-6
-const room5 = await Room.findOne({_id : "6672f5a8424b440bf6b30186"}); //E°SO 이소하우스 60평 독채-15
-const room6 = await Room.findOne({_id : "6672f5a8424b440bf6b301b8"}); //Panorama-Full오션뷰-2
-const room7 = await Room.findOne({_id : "6672f5a8424b440bf6b3019f"}); //청초淸楚 _ 위드 오션-2
-const room8 = await Room.findOne({_id : "6672f5a8424b440bf6b301d1"}); //ilsanghosa A + B: Architect's Luxury Beach House -12
-const room9 = await Room.findOne({_id : "6672f5a8424b440bf6b301f0"}); //뜨레시옷송당 제주키즈 풀빌라 -7
-const room10 = await Room.findOne({_id : "6672f5a8424b440bf6b30209"}); //스테이 재 -3
+const room1 = await Room.findOne({_id : "6676e812a1cf2a45e7d3fa1f"}); //위드독 독채 풀빌라 -6
+const room2 = await Room.findOne({_id : "6676e812a1cf2a45e7d3fa2e"}); //강화도 럭셔리 대형 풀빌라-16
+const room3 = await Room.findOne({_id : "6676e812a1cf2a45e7d3fa3b"}); //다온빌(스파/개별풀)-10
+const room4 = await Room.findOne({_id : "6676e812a1cf2a45e7d3fa77"}); //바닷가 바로앞 오션뷰스파 객실, b200-6
+const room5 = await Room.findOne({_id : "6676e812a1cf2a45e7d3fa85"}); //E°SO 이소하우스 60평 독채-15
+const room6 = await Room.findOne({_id : "6676e812a1cf2a45e7d3fa9f"}); //Panorama-Full오션뷰-2
+const room7 = await Room.findOne({_id : "6676e812a1cf2a45e7d3fa92"}); //청초淸楚 _ 위드 오션-2
+const room8 = await Room.findOne({_id : "6676e812a1cf2a45e7d3faac"}); //ilsanghosa A + B: Architect's Luxury Beach House -12
+const room9 = await Room.findOne({_id : "6676e812a1cf2a45e7d3fabc"}); //뜨레시옷송당 제주키즈 풀빌라 -7
+const room10 = await Room.findOne({_id : "6676e812a1cf2a45e7d3fac9"}); //스테이 재 -3
 
 
 // 유저 정보 조회
@@ -36,8 +36,8 @@ console.log(bok1);
 
 
 const booking_Insert = await Booking.create(
-    //위드독 독채 풀빌라 예약건 -2건
-    {
+     // user1 - room 1,6,2,10
+     {
         userId : user1,
         roomId : room1,
         checkInDate : "2024.06.19",
@@ -46,16 +46,33 @@ const booking_Insert = await Booking.create(
         infants : 1,
     },
     {
-        userId : user2,
-        roomId : room1,
-        checkInDate : "2024.06.22",
-        checkOutDate : "2024.06.24",
+        userId : user1,
+        roomId : room6,
+        checkInDate : "2024.06.20",
+        checkOutDate : "2024.06.22",
         guests : 2,
         infants : 0,
     },
-    //강화도 럭셔리 대형 풀빌라 예약건 -2건
     {
-        userId : user3,
+        userId : user1,
+        roomId : room2,
+        checkInDate : "2024.06.23",
+        checkOutDate : "2024.06.24",
+        guests : 10,
+        infants : 1,
+    },
+    {
+        userId : user1,
+        roomId : room10,
+        checkInDate : "2024.07.01",
+        checkOutDate : "2024.07.04",
+        guests : 2,
+        infants : 1,
+    },
+    
+    // user2 - room 1,2,6
+    {
+        userId : user2,
         roomId : room2,
         checkInDate : "2024.06.20",
         checkOutDate : "2024.06.22",
@@ -63,81 +80,13 @@ const booking_Insert = await Booking.create(
         infants : 2,
     },
     {
-        userId : user4,
-        roomId : room2,
-        checkInDate : "2024.06.23",
-        checkOutDate : "2024.06.24",
-        guests : 10,
-        infants : 1,
-    },
-    //다온빌(스파/개별풀) 예약건 -3건
-    {
-        userId : user5,
-        roomId : room3,
-        checkInDate : "2024.06.21",
-        checkOutDate : "2024.06.22",
-        guests : 8,
-        infants : 2,
-    },
-    {
-        userId : user6,
-        roomId : room3,
-        checkInDate : "2024.06.23",
-        checkOutDate : "2024.06.27",
-        guests : 7,
-        infants : 2,
-    },
-    {
-        userId : user7,
-        roomId : room3,
-        checkInDate : "2024.06.27",
-        checkOutDate : "2024.06.29",
-        guests : 6,
-        infants : 2,
-    },
-       //바닷가 바로앞 오션뷰스파 객실, b200 예약건 -3건
-       {
-        userId : user8,
-        roomId : room4,
+        userId : user2,
+        roomId : room1,
         checkInDate : "2024.06.22",
-        checkOutDate : "2024.06.26",
-        guests : 4,
-        infants : 0,
-    },
-    {
-        userId : user9,
-        roomId : room4,
-        checkInDate : "2024.06.26",
-        checkOutDate : "2024.06.27",
-        guests : 2,
-        infants : 2,
-    },
-    {
-        userId : user10,
-        roomId : room4,
-        checkInDate : "2024.06.27",
-        checkOutDate : "2024.06.30",
-        guests : 5,
-        infants : 1,
-    },
-    //°SO 이소하우스 60평 독채, b200 예약건 -1건
-    {
-        userId : user4,
-        roomId : room5,
-        checkInDate : "2024.06.26",
-        checkOutDate : "2024.06.28",
-        guests : 10,
-        infants : 5,
-    },
-    // Panorama-Full오션뷰 예약건 -3건
-    {
-        userId : user1,
-        roomId : room6,
-        checkInDate : "2024.06.25",
-        checkOutDate : "2024.06.26",
+        checkOutDate : "2024.06.24",
         guests : 2,
         infants : 0,
-    },
+    },  
     {
         userId : user2,
         roomId : room6,
@@ -145,6 +94,32 @@ const booking_Insert = await Booking.create(
         checkOutDate : "2024.06.28",
         guests : 2,
         infants : 0,
+    },
+
+// user3 - room 3,4,5,6,7,8
+    {
+        userId : user3,
+        roomId : room3,
+        checkInDate : "2024.06.21",
+        checkOutDate : "2024.06.22",
+        guests : 8,
+        infants : 2,
+    },
+    {
+        userId : user3,
+        roomId : room4,
+        checkInDate : "2024.06.22",
+        checkOutDate : "2024.06.26",
+        guests : 4,
+        infants : 0,
+    },
+    {
+        userId : user3,
+        roomId : room5,
+        checkInDate : "2024.06.26",
+        checkOutDate : "2024.06.28",
+        guests : 10,
+        infants : 5,
     },
     {
         userId : user3,
@@ -154,9 +129,8 @@ const booking_Insert = await Booking.create(
         guests : 2,
         infants : 0,
     },
-    // 청초淸楚 _ 위드 오션 예약건 -2건
     {
-        userId : user4,
+        userId : user3,
         roomId : room7,
         checkInDate : "2024.07.01",
         checkOutDate : "2024.07.03",
@@ -164,33 +138,17 @@ const booking_Insert = await Booking.create(
         infants : 0,
     },
     {
-        userId : user5,
-        roomId : room6,
-        checkInDate : "2024.07.03",
-        checkOutDate : "2024.07.04",
-        guests : 1,
-        infants : 1,
-    },
-    // ilsanghosa A + B: Architect's Luxury Beach House 예약건 -2건
-    {
         userId : user3,
-        roomId : room8,
-        checkInDate : "2024.07.01",
-        checkOutDate : "2024.07.02",
-        guests : 10,
-        infants : 2,
-    },
-    {
-        userId : user4,
         roomId : room8,
         checkInDate : "2024.07.03",
         checkOutDate : "2024.07.10",
         guests : 8,
         infants : 2,
     },
-    //뜨레시옷송당 제주키즈 풀빌라 예약건 -2건
+
+    //user4 -room 9,10
     {
-        userId : user8,
+        userId : user4,
         roomId : room9,
         checkInDate : "2024.07.02",
         checkOutDate : "2024.07.03",
@@ -198,24 +156,7 @@ const booking_Insert = await Booking.create(
         infants : 0,
     },
     {
-        userId : user9,
-        roomId : room9,
-        checkInDate : "2024.07.03",
-        checkOutDate : "2024.07.07",
-        guests : 4,
-        infants : 2,
-    },
-    //스테이 재 예약건 - 3건
-    {
-        userId : user1,
-        roomId : room10,
-        checkInDate : "2024.07.10",
-        checkOutDate : "2024.07.11",
-        guests : 2,
-        infants : 1,
-    },
-    {
-        userId : user9,
+        userId : user4,
         roomId : room10,
         checkInDate : "2024.07.11",
         checkOutDate : "2024.07.13",
