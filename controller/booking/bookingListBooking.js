9e02eb72e04faaded4c18303270eae26bd7bb0ba
maruanna <maruanna1994@gmail.com>
2024-06-27 14:47:39 +0900

    for pull

--- controller/booking/bookingListBooking.js
+++ controller/booking/bookingListBooking.js
@@ -2,12 +2,7 @@ import Room from "../../models/roomSchema.js";
 
 const bookingListBooking = async (req, res) => {
 
-    const room = await Room.findOne(
-            {
-                "_id" : req.query.roomId
-            }
-    );
- 
+
 
 
 
--- controller/room/wishListRoom.js
+++ controller/room/wishListRoom.js
@@ -2,42 +2,34 @@ import mongoose from "mongoose";
 import Room from "../../models/roomSchema.js";
 import User from "../../models/userSchema.js";
 
-
-
 const wishListRoom = async (req, res) => {
-
     try {
-        const userId = req.query.userId;
-
-        // userId를 ObjectId로 변환합니다.
-        const objectId = new mongoose.Types.ObjectId(userId);
-
-        // 유저 정보를 가져옵니다.
-        const user = await User.findOne({ _id: objectId }).populate("wishList");
+        // user 조회 (wishList 참조객체도 같이)
+        const user = await User.findOne({ userId: req.query.userId }).populate("wishList");
 
         if (!user) {
             console.log("User not found");
             return res.status(404).json({ message: "User not found" });
         }
-
-        // 유저 이름을 콘솔에 출력합니다.
+        
+        // check
         console.log("User Name:", user.name);
 
-        // 유저의 wishList에 있는 모든 방을 가져옵니다.
+        // 찜한 숙소 다 가져오기 (array)
         const rooms = user.wishList;
 
-        // 방 제목을 콘솔에 출력합니다.
+        // check
         rooms.forEach(room => {
             console.log("Room Title:", room.title);
         });
 
-        // 클라이언트에게 방 정보를 응답합니다.
+        // client에 room정보 보내기
         res.status(200).json({ rooms: rooms });
+
     } catch (error) {
-        console.error("Error fetching wish list:", error);
+        console.error("Error fetching wishList:", error);
         res.status(500).json({ message: error.message });
     }
 };
-
 
 export default wishListRoom;
