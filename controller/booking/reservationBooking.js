8007897f3b899a3a6bd79e8a24575af4f7f18154
양승민 <746aa1357@gmail.com>
2024-07-03 16:26:30 +0900

    roomdata

--- controller/booking/reservationBooking.js
+++ controller/booking/reservationBooking.js
@@ -1,3 +1,17 @@
-const reservationBooking = async (req, res) => {};
+import Room from "../../models/roomSchema.js";
+
+
+const reservationBooking = async (req, res) => {
+    try{
+
+        const reservation = await Room.find({ _id: req.body.roomId});
+        res.json(reservation);
+        
+    } catch(error) {
+        console.error("Error fetching reservation:", error);
+        res.status(500).json({message:error.message});
+
+    }
+};
 
 export default reservationBooking;
