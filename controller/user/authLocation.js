d9435e3763e75f684184bab85730abd5b6eb2fce
19parke <19parke@gmail.com>
2024-07-04 21:20:50 +0900

    401 code error 해결

--- controller/user/authLocation.js
+++ controller/user/authLocation.js
@@ -1,15 +1,24 @@
 // 토큰을 이용해서 인증받은 라우팅
 const authLocation = async (req, res, next) => {
+    console.log(req)
     try {
-    // 인가가 완료된 유저는 req.user 담긴다.
-    const {password, ...others} = req.user._doc;
-    res.status(200).json({
-        message : '자동 로그인 성공',
-        ...others,
-    });
+        if(!req.user || !req.user._doc){
+            return res.status(401).json({
+                message: 'Unauthorized',
+            })
+        }
+        // 인가가 완료된 유저는 req.user 담긴다.
+        const {password, ...others} = req.user._doc;
+        res.status(200).json({
+            message : '자동 로그인 성공',
+            ...others,
+        });
     } catch (error) {
         console.error(error)
         next(error)
+        res.status(401).json({
+            message: 'Unauthorized',
+        });
     }
 }
 
